3fdb8a67762ca1280b7bf0762c7c6b684f9a0f53
Aboudebug <djarimias@gmail.com>
2024-07-11 15:44:55 +0200

    Short responsive

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ export function App() {
             Your All-in-One Solution!</h1>
         </div>
         <div className="flex justify-center">
-            <div className="grid grid-cols-2 gap-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
          {
             cards.map((card, index) => {
                 return <Card 
